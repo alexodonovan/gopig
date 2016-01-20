a8ab926800896ee3f33c70d9017ef9a8c44355d6
Alex O'Donovan <alexodonovan@msn.com>
2016-01-20 21:20:21 +0000

    start the application on the raspberry pi after deploy.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 mvn clean install;
 scp target/gopig-1.0-SNAPSHOT.one-jar.jar pi@192.168.1.7:~/development/gopig/target;
+ssh pi@192.168.1.7 << EOF
+	git pull;
+	cd ~/development/gopig;
+	sh run.sh;
+EOF
