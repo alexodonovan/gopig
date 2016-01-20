ff00fa4b47825e3b57995e2c49046306917050d6
Alex O'Donovan <alexodonovan@msn.com>
2016-01-20 21:24:24 +0000

    Deploy script updates.
    Comment out the start app in deploy script, until something like
    mosquitto installed.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 mvn clean install;
 scp target/gopig-1.0-SNAPSHOT.one-jar.jar pi@192.168.1.7:~/development/gopig/target;
-ssh pi@192.168.1.7 << EOF
-	git pull;
-	cd ~/development/gopig;
-	sh run.sh;
-EOF
+
+# add the below lines to execute the application on the raspberry pi on deploy
+# ssh pi@192.168.1.7 << EOF
+#	git pull;
+#	cd ~/development/gopig;
+#	sh run.sh;
+# EOF
