8e36bf81fd227b74a05565770b54ea8418af93dd
Alex O'Donovan <alexodonovan@msn.com>
2016-02-03 23:18:07 +0000

    add jackson subclass

--- src/main/java/com/oddsocks/gopig/messaging/Command.java
+++ src/main/java/com/oddsocks/gopig/messaging/Command.java
@@ -1,5 +1,12 @@
 package com.oddsocks.gopig.messaging;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = As.PROPERTY, property = "clzType")
+@JsonSubTypes({ @Type(ForwardCommand.class), @Type(StopCommand.class) })
 public abstract class Command {
 
 }
