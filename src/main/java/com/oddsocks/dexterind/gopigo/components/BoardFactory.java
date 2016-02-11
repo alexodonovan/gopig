e63f263c770ca17926602ae4c8987e20cf580c8a
Alex O'Donovan <alexodonovan@msn.com>
2016-02-11 22:08:47 +0000

    SCA fix

--- src/main/java/com/oddsocks/dexterind/gopigo/components/BoardFactory.java
+++ src/main/java/com/oddsocks/dexterind/gopigo/components/BoardFactory.java
@@ -6,17 +6,17 @@ import com.oddsocks.dexterind.gopigo.Gopigo;
 import com.pi4j.io.i2c.I2CBus;
 
 public class BoardFactory {
+	
+	/**
+	 * The device's address.
+	 */
+	private static final byte ADDRESS = 0x08;
 
 	private final Gopigo gopigo;
 
 	public BoardFactory(Gopigo gopigo) {
 		this.gopigo = gopigo;
 	}
-
-	/**
-	 * The device's address.
-	 */
-	private static final byte ADDRESS = 0x08;
 
 	public Board createBoard(I2CBus bus) throws IOException, InterruptedException {
 		return new Board(bus.getDevice(ADDRESS), gopigo);
